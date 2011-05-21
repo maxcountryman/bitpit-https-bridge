08034001c8dc32d6f57c91c707af4b8d18d644be
Max Countryman <max.countryman@gmail.com>
2011-05-20 23:24:23 -0700

    remove assert for now

--- httpstobitpit/views.py
+++ httpstobitpit/views.py
@@ -3,22 +3,20 @@ import urllib
 from httpstobitpit import app
 from flask import Flask, render_template, make_response, request, abort 
 
-API_URL = 'http://api.bitp.it/work?'
-
 # views
 @app.route('/work')
 def index():
     '''Wrapper for bitp.it API'''
     
+    url = 'http://api.bitp.it/work?'
+    
     client_id = request.args.get('client_id', '')
     hash_rate = request.args.get('hash_rate', '')
+    params = [client_id, hash_rate]
     #assert client_id and hash_rate or abort(400)
     
-    params = \
-            'client_id={0}'.format(client_id) \
-            + '&' + \
-            'hash_rate={0}'.format(hash_rate)
-    url = API_URL + params
+    for param in params:
+        url += param + '=' + params[params.index(param)] + '&';
     
     response = make_response(urllib.urlopen(url).read())
     response.content_type = 'application/json'
